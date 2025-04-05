82418e329a13f6f02c33acbd02d8a2408728851f
luanmvcosta0 <luanmvcosta@gmail.com>
2025-04-05 10:58:10 -0300

    Criando o componente despesas no meu modulo lancamentos

---
+++ src/app/lancamentos/despesas/despesas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-despesas',
+  imports: [],
+  templateUrl: './despesas.component.html',
+  styleUrl: './despesas.component.scss'
+})
+export class DespesasComponent {
+
+}
