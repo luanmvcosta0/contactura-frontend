6e4ca173342b31bb6a364c5966a76599c7ecd2a1
luanmvcosta0 <luanmvcosta@gmail.com>
2025-04-05 14:05:45 -0300

    Adicionando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'login', component: LoginComponent},
+    {path: 'cadastro', component: CadastroComponent},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: '**', component: PageNotFoundComponent}
+];
